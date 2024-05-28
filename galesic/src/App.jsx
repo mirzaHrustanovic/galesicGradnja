c1b867ee0a926ce5ed275025817b40d8247fa59e
Mirza <mirza-hrustanovic@hotmail.com>
2024-05-28 15:25:09 +0200

    added in style to information class of header section

--- galesic/src/App.jsx
+++ galesic/src/App.jsx
@@ -1,4 +1,5 @@
 import Home from "./components/pages/Home"
+import "./App.css"
 function App() {
  
   return (
--- galesic/src/components/Header.jsx
+++ galesic/src/components/Header.jsx
@@ -3,22 +3,26 @@ import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import { faPhoneVolume } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import Flag from 'react-flagkit';
+import logo from "../assets/Images/logo/muhasLogo4.jpg"
+import "./styles/Header.css"
 
 const Header = () => {
   return (
     <>
-      <header>
+      <header className="main-header">
         <div className="information">
-            <div className="location"><FontAwesomeIcon icon={faLocationDot} size="2xl"/><h3>Momanovo b.b.</h3></div>
+            <div className="location"><FontAwesomeIcon icon={faLocationDot} size="2xl"/><h3>Slavinovickog odreda b.b.</h3></div>
             <div className="number"><FontAwesomeIcon icon={faPhoneVolume} size="2xl"/><h3>+38762532542</h3></div>
             <div className="email"><FontAwesomeIcon icon={faEnvelope} size="2xl"/><h3>elektrotettnang@gmail.com</h3></div>
-            <div><Flag country="BA" role="button" onClick={() =>{
+           <div className="flags">
+            <div className="bosnian-flag"><Flag country="BA" role="button" onClick={() =>{
                 alert("You have clicked bosnian flag")
-            }}/></div>
-            <div><Flag country="DE"/></div>
-            <div><Flag country="GB"/></div>
+            }} /></div>
+            <div className="german-flag"><Flag country="DE"/></div>
+            <div className="english-flag"><Flag country="GB"/></div>
         </div>
-        <div>Logo</div>
+        </div>
+        <div className="main-header__logo"><img src={logo} alt="logo" height={80} width={100}/></div>
         <nav>
           <ul>
             <li>Home</li>
