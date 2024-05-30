455f9fadebd5353a7feec762d096488a216e3330
Mirza <mirza-hrustanovic@hotmail.com>
2024-05-31 01:29:50 +0200

    added about page and style it almost

--- galesic/src/components/pages/About.jsx
+++ galesic/src/components/pages/About.jsx
@@ -1,11 +1,16 @@
-
-
+import "../styles/About.css";
 const About = () => {
   return (
-    <div>
+    <div className="about">
       <h1>O nama</h1>
+      <p>
+        Galešić Gradnja d.o.o. je porodična građevinska firma koja se bavi svim
+        vrstama unutrašnjih i vanjskih radova u kući i oko kuće. S više od 20
+        godina iskustva pružamo vrhunske građevinske usluge koje garantuju
+        kvalitet i dugotrajnost.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
